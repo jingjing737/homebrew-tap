01d2f9b218b2d7ff2d1d7bc306fcd20305230db7
jingjing737 <jingjing737@github.com>
2026-07-22 08:11:16 +0800

    fix sha256 for v3.5.0

--- devtools-hub.rb
+++ devtools-hub.rb
@@ -2,7 +2,7 @@ class DevtoolsHub < Formula
   desc "Developer toolkit for system monitoring"
   homepage "https://github.com/jingjing737/devtools-hub"
   url "https://github.com/jingjing737/devtools-hub/archive/refs/tags/v3.5.0.tar.gz"
-  sha256 "aa756c66f0540af6610be799e0a24d66481a91d4a129e824349b21f79e68c9ca"
+  sha256 "582117edaa08a7672b9df49e76c55e12f9f560cbd8dccead65d86fb8fd93f908"
   license "MIT"
   version "3.5.0"
 
